202005bb4d0fdbbd4a38a74689bd657dbad309ec
mkozak0108 <mkozak0108@gmail.com>
2014-12-25 12:16:08 +0200

    Added images. Find width functions.

--- client/app/js/main.js
+++ client/app/js/main.js
@@ -1,14 +1,27 @@
-Session.setDefault("counter", 0);
+document.addEventListener('click', function (e) {
+  var lightning = document.querySelector('.lightning'),
+    core = document.querySelector('.core'),
+    width = findWidth({x: e.pageX, y: e.pageY}, {x: core.getBoundingClientRect().x, y: core.getBoundingClientRect().y});
 
-Template.hello.helpers({
-  counter: function () {
-    return Session.get("counter");
+  if (width > 110) {
+    lightning.style.width =  Math.round(width/2) + 'px';
+    animate(lightning, 150);
   }
 });
 
-Template.hello.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    Session.set("counter", Session.get("counter") + 1);
-  }
-});
+function animate (element, interval) {
+  element.classList.remove('hidden');
+  setTimeout(function() {
+    element.classList.add('hidden');
+  }, interval);
+
+  return element;
+}
+
+function findWidth (firstPoint, secondPoint) {
+  var result, widthX, widthY;
+  widthX = firstPoint.x - secondPoint.x;
+  widthY = firstPoint.y - secondPoint.y;
+  result = Math.pow(Math.pow(widthX, 2) + Math.pow(widthY, 2), 0.5);
+  return result;
+}
